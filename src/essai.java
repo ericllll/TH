a025101b032a704ad7e12d5c81b66a851c2bde4e
ericllll <ericllll@outlook.com>
2015-03-22 17:09:30 +0100

    Modification mineure du premier initial

--- src/essai.java
+++ src/essai.java
@@ -1,12 +1,12 @@
-import java.io.File;
-import java.io.IOException;
+//import java.io.File;
+//import java.io.IOException;
 //import com.healthmarketscience.jackcess.*;
 
 public class essai {
 //Table table;
 //Database base;
 	public static void main(String[] args) {
-		System.out.println("done");
+		System.out.println("Début");
 	}
 
 }
